44b10afd5da1e4cbc658ff8f77684fafda68eb2e
bluedevil-oit <ed151@duke.edu>
2019-04-23 20:22:50 -0400

    updated celery install

--- contrib/install/ubuntu-16LTS/runcelery.sh
+++ contrib/install/ubuntu-16LTS/runcelery.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # NOTE: this script is used by the celery systemd service to run celery in the MDCS virtual environment
+source ./mdcs_vars # HACK!
 source ${MDCS_HOME}/${MDCS_VENV}/bin/activate
-/bin/sh ${MDCS_CELERY_BIN} $*
+/usr/bin/env bash -c "${MDCS_CELERY_BIN} $*"
 
